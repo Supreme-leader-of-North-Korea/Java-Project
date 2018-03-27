53e302d55b1a149cdbeb567973d8e856b6f1d351
THGAloeVeraX <arne.95@hotmail.com>
2018-03-27 19:34:05 +0800

    Initial Guest class update
    Changed credit card details and contact to String data type due to possible integer overflow using 32-bit Int, furthermore no arithmetic will be done to said variables.

--- src/Guest.java
+++ src/Guest.java
@@ -2,30 +2,43 @@ import java.util.Scanner;
 
 
 public class Guest {
-    private String name, address, country, gender, nationality, identity;
-    private int credit, contact;
+	
+	//Attributes
+    private String name, creditDetails, address, country, gender, nationality, identity, contact;
     
-    public Guest(){
-        
-    }
-    
-    public void create() {
+    //Constructor with ID as primary key
+    public Guest(String identity) {
+		super();
+		this.identity = identity;
+	}
+
+	public void create() {
         Scanner s = new Scanner(System.in);
+        
         System.out.println("Please enter your name: ");
         name = s.nextLine();
+        
 		System.out.println("Please enter your address: ");
 		address = s.nextLine();
+		
 		System.out.println("Please enter your country: ");
 		country = s.nextLine();
+		
 		System.out.println("Please enter your gender: ");
 		gender = s.next();
+		
 		System.out.println("Please enter your nationality: ");
 		nationality = s.nextLine();
+		
 		System.out.println("Please enter your identity[(D)riving License/(P)assport]: ");
 		identity = s.nextLine();
+		
 		System.out.println("Please enter your credit card detail: ");
-		credit = s.nextInt();
+		creditDetails = s.nextLine();
+		
 		System.out.println("Please enter your contact number: ");
-		contact = s.nextInt();
+		contact = s.nextLine();
+		
+		s.close();
     }
 }
