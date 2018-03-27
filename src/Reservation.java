f5986594517e31b268a4e5ecf463e509057bcacd
Supreme-leader-of-North-Korea <37832470+Supreme-leader-of-North-Korea@users.noreply.github.com>
2018-03-27 20:18:41 +0800

    Update Reservation.java

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,4 +1,44 @@
 
 public class Reservation {
+  public enum ReservationStatus {
+			CONFIRM, IN_WAITLIST, CHECK_IN, EXPIRED
+		}
+		
+		//Attributes
+		private int roomId;
+		private int customerName;
+		private RoomStatus roomStatus;
+    private int checkInDate;
+		
+		//Methods
+		public Reservation(int roomId, int customerName, String checkInDate){
+			this.roomId = roomId;
+			this.customerName = customerName;
+			this.checkInDate = checkInDate;
+		}
+		
+		//Getter and setters
+		public int getRoomId() {	
+			return roomId;
+		}
+		
+		public int getCustomerName() {
+			return customerName;
+		}
+		
+		public RoomStatus getRoomStatus() {
+			return roomStatus;
+		}
 
+		public void setRoomId(int roomId) {
+			this.roomId = roomId;
+		}
+
+		public void setCustomerName(int customerName) {
+			this.customerName = customerName;
+		}
+
+		public void setRoomStatus(RoomStatus roomStatus) {
+			this.roomStatus = roomStatus;
+		}	
 }
