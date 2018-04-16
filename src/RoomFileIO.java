3f3d429cb36aff9a0cba842458db140724a13370
oysterly <37858934+oysterly@users.noreply.github.com>
2018-04-16 11:22:55 +0800

    Add files via upload

--- src/RoomFileIO.java
+++ src/RoomFileIO.java
@@ -38,13 +38,13 @@ public class RoomFileIO extends FileIO<Room>{
 				Room temp;
 
 				switch (arr[7]) {
-				case "single":	temp = new Room_single (arr[0], arr[1], Room.strToBedType(arr[2]), Menu.dateConvert(arr[3]), Menu.dateConvert(arr[4]), arr[5], Room.strToRoomStatus(arr[6]), arr[7]);
+				case "single":	temp = new Room_single (arr[0], arr[1], Room.strToBedType(arr[2]), Menu.strToDate(arr[3]), Menu.strToDate(arr[4]), arr[5], Room.strToRoomStatus(arr[6]), arr[7]);
 								break;
-				case "double":	temp = new Room_double (arr[0], arr[1], Room.strToBedType(arr[2]), Menu.dateConvert(arr[3]), Menu.dateConvert(arr[4]), arr[5], Room.strToRoomStatus(arr[6]), arr[7]);
+				case "double":	temp = new Room_double (arr[0], arr[1], Room.strToBedType(arr[2]), Menu.strToDate(arr[3]), Menu.strToDate(arr[4]), arr[5], Room.strToRoomStatus(arr[6]), arr[7]);
 								break;
-				case "deluxe":	temp = new Room_deluxe (arr[0], arr[1], Room.strToBedType(arr[2]), Menu.dateConvert(arr[3]), Menu.dateConvert(arr[4]), arr[5], Room.strToRoomStatus(arr[6]), arr[7]);
+				case "deluxe":	temp = new Room_deluxe (arr[0], arr[1], Room.strToBedType(arr[2]), Menu.strToDate(arr[3]), Menu.strToDate(arr[4]), arr[5], Room.strToRoomStatus(arr[6]), arr[7]);
 								break;
-				default:		temp = new Room_vip (arr[0], arr[1], Room.strToBedType(arr[2]), Menu.dateConvert(arr[3]), Menu.dateConvert(arr[4]), arr[5], Room.strToRoomStatus(arr[6]), arr[7]);			   				
+				default:		temp = new Room_vip (arr[0], arr[1], Room.strToBedType(arr[2]), Menu.strToDate(arr[3]), Menu.strToDate(arr[4]), arr[5], Room.strToRoomStatus(arr[6]), arr[7]);			   				
 								break;
 				}
 				rlist.add(temp);
